bd7016910ece77d7c1e3c26e1c7a76821c5e48ce
Max Gurela <maxpowa1@gmail.com>
2016-07-15 00:51:35 -0700

    Site serves working pages \o/

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.set('subdomain offset', 1);
+//app.set('subdomain offset', 1);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,20 @@ let express = require('express')
 let router = express.Router()
 let danbooru = require('danbooru')
 
+let shuffle = (array) => {
+  let i = 0, j = 0, temp = null
+
+  for (i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1))
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+}
+
 let handle_request = (req, res, next) => {
   // Skip this request as it's a subpath on the root
-  if (req.subdomains.length === 0) next()
+  if (req.subdomains.length === 0) return next()
 
   let subject = req.subdomains.reverse().join(" ")
   subject = subject.replace(/(^|\s)[a-z]/g, (f) => f.toUpperCase() )
@@ -14,14 +25,24 @@ let handle_request = (req, res, next) => {
   let tags = req.path.replace(/^\//, '')
 
   tags = req.path.split('/')
-  tags.push(subject.replace(" ", "_"))
+  // Add * to subject for wildcard danbooru search (misaka will find misaka_mikoto etc)
+  tags.push(subject.replace(" ", "_") + "*")
 
 
-  danbooru.search(tags.join(" "), {limit: 20}, (err, pageData) => {
+  danbooru.search(tags.join(" "), {limit: 50}, (err, pageData) => {
     if (err) throw err;
-    if (!pageData) throw new Error()
+    if (pageData.length === 0) {
+      var error = new Error('Not found')
+      error.status = 404
+      return next(error)
+    }
 
-    res.render('index', { title: tags.join(" "), image_url: pageData.random().preview_file_url })
+    let previews = pageData.map((val) => {
+      return val.preview_file_url
+    })
+    shuffle(previews)
+
+    res.render('index', { subject: subject, previews: previews})
   })
 }
 
